d2e4773e3c74a300b953632d6370ef83e669295c
Adebayo David <brilldavid85@yahoo.com>
2025-02-15 10:45:14 +0100

    updated

--- models/Order.js
+++ models/Order.js
@@ -1,34 +1,77 @@
 const mongoose = require("mongoose");
 
-const OrderSchema = new mongoose.Schema(
-  {
-    items: [
-      {
-        itemId: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Item",
-          required: true,
-        },
-        quantity: {
-          type: Number,
-          default: 1,
-        },
-      },
-    ],
-    amount: {
-      type: Number,
-      required: true,
-    },
-    address: {
-      type: Object,
-      required: true,
-    },
-    status: {
-      type: String,
-      default: "pending",
-    },
+const AddressSchema = new mongoose.Schema({
+  name: { 
+    type: String, 
+    required: true 
   },
-  { timestamps: true }
-);
+  street: { 
+    type: String, 
+    required: true 
+  },
+  area: { 
+    type: String, 
+    required: true 
+  },
+  city: { 
+    type: String, 
+    required: true 
+  },
+  zipCode: { 
+    type: String, 
+    required: true 
+  }
+});
+
+const OrderSchema = new mongoose.Schema({
+  items: [{
+    itemId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Item",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      default: 1,
+    },
+    warranty: {
+      type: Number,
+      default: 3
+    }
+  }],
+  itemsAndInstallation: {
+    type: Number,
+    required: true,
+  },
+  vat: {
+    type: Number,
+    required: true,
+  },
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
+  address: {
+    type: AddressSchema,
+    required: true
+  },
+  paymentMethod: {
+    type: String,
+    enum: ['visa', 'pay-small-small'],
+    required: true
+  },
+  paymentDetails: {
+    cardLastFour: String,
+  },
+  installationSchedule: {
+    earliestDate: Date,
+    maxDuration: String
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'confirmed', 'installing', 'completed', 'cancelled'],
+    default: "pending",
+  }
+}, { timestamps: true });
 
 module.exports = mongoose.model("Order", OrderSchema);
